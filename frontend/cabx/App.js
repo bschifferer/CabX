f1dd628db4d8900c224e6ff6fc63d5a1856d9da3
Clyde Bazile <clydebazile@dyn-160-39-143-93.dyn.columbia.edu>
2018-10-26 17:31:39 -0400

    add list of uber/lyft prices

--- frontend/cabx/App.js
+++ frontend/cabx/App.js
@@ -1,32 +1,79 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { SearchBar, ListItem } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+const list = [
+  {
+    type: 'Uber Pool',
+    price: 2.0,
+    eta: '4:25pm',
+    service: 'Uber',
+    icon: 'refresh',
+  },
+  {
+    type: 'Uber X',
+    price: 5.0,
+    eta: '4:30pm',
+    service: 'Uber',
+    icon: 'refresh',
+  },
+  {
+    type: 'Uber Pool',
+    price: 1.05,
+    eta: '4:25pm',
+    service: 'Uber',
+    icon: 'refresh',
+  },
+  {
+      type: 'Uber X',
+    price: 50.0,
+    eta: '4:30pm',
+    service: 'Uber',
+    icon: 'refresh',
+  }
+]
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <SearchBar
+        showLoading
+        platform="ios"
+        cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+        placeholder='From...' />
+        
 		<SearchBar
-		showLoading
-		platform="ios"
-		cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-		placeholder='From...' />
+        showLoading
+        platform="ios"
+        cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+        placeholder='To...' />
 
-		<SearchBar
-		showLoading
-		platform="ios"
-		cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-		placeholder='To...' />
+		{
+			list.map((item) => (
+			<ListItem
+			key={item}
+			leftIcon={ {name: item.icon }}
+			badge={{ value: "$" + item.price, textStyle: { color: 'orange' }, containerStyle: { marginTop: -20 } }}
+			title={item.type}
+			subtitle={item.eta}
+			hideChevron
+			/>
+		))
+		}
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'column',
-		backgroundColor: 'white',
-		marginTop: 40,
-	},
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        marginTop: 40,
+    },
 });
