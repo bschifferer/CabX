58c555390b92af400f217c3a3e93e70a7938ae92
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-10-30 09:41:36 -0400

    fixed push.shj

--- push.sh
+++ push.sh
@@ -6,7 +6,7 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout coverage_reports
+  git checkout -b coverage_reports
   mkdir -p "coverage_reports" 
   if [ -z "$TRAVIS_BUILD_NUMBER" ]
     then
