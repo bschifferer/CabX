0271e4b66a9484051e649e9837785c2646182266
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-10-30 09:13:00 -0400

    added push to travis ci

--- push.sh
+++ push.sh
@@ -18,7 +18,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https:/${GH_TOKEN}/@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
   git push --quiet --set-upstream origin gh-pages
 }
 
