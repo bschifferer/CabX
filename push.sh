e4f0b350993b5525edae975c1d449efc1c55518d
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-10-30 09:09:09 -0400

    change push.sh

--- push.sh
+++ push.sh
@@ -18,7 +18,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://53f21ca7837c0f1bdd16374cca8e9c77aafe3090@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
+  git remote add origin https:/${GH_TOKEN}/@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
   git push --quiet --set-upstream origin gh-pages 
 }
 
