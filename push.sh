8a31763e363a555e6979d2c5a48097535e55dde9
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-10-30 09:51:50 -0400

    test access

--- push.sh
+++ push.sh
@@ -19,6 +19,7 @@ commit_website_files() {
 }
 
 upload_files() {
+  echo ${GH_TOKEN}
   git remote add origin https://${GH_TOKEN}@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
   git push --quiet --set-upstream origin coverage_reports 
 }
