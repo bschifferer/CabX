49b82b3a7947f0fc38657c710a52269bc872cbb2
Clyde Bazile <clydebazile@Clydes-MacBook-Pro.local>
2018-10-31 18:04:03 -0400

    Clean UI and add sorter

--- frontend/cabx/App.js
+++ frontend/cabx/App.js
@@ -1,97 +1,137 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Card, SearchBar, ListItem } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Card, SearchBar, ListItem, List } from 'react-native-elements';
+import { Container, Content, Tabs, Tab, TabHeading, Item, Input, Header, Title, Button, Icon, Left, Right, Body } from "native-base";
+
+const uberIcon = 'https://banner2.kisspng.com/20180715/fje/kisspng-computer-icons-user-clip-art-uber-logo-transparent-5b4b0088e1dc35.0486796815316419929251.jpg';
+const lyftIcon = 'http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c525.png';
+
+const price = 0;
+const time = 1;
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toAddress:'', 
-      fromAddress:'',
-      buttonPress: false,
-      data: [],
-    };
-    this.resolveAddress = this.resolveAddress.bind(this);
-  }
 
-  resolveAddress(fromAddress, toAddress) {
-    fetch('http://ec2-18-215-158-47.compute-1.amazonaws.com:3000/findRides/', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "from": fromAddress,
-        "to": toAddress
-      }),
-    }).then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({ data: responseJson, buttonPress: false })
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			toAddress:'', 
+			fromAddress:'',
+			buttonPress: false,
+			data: [],
+			sortBy: price,
+		};
+		this.resolveAddress = this.resolveAddress.bind(this);
+		this.swapAddress = this.swapAddress.bind(this);
+		this.changeTab = this.changeTab.bind(this);
+		this.getSortedData = this.getSortedData.bind(this);
+	}
 
-  render() {
-	  // console.log("beginning")
-   //  console.log(this.state.fromAddress)
-   //  console.log(this.state.toAddress)
-   //  console.log(this.state.buttonPress)
+	resolveAddress(fromAddress, toAddress) {
+		fetch('http://ec2-18-215-158-47.compute-1.amazonaws.com:3000/findRides/', {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				"from": fromAddress,
+				"to": toAddress
+			}),
+		}).then((response) => response.json())
+			.then((responseJson) => {
+				this.setState({ data: responseJson, buttonPress: false })
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}
 
-    if (this.state.buttonPress) {
-      this.resolveAddress(this.state.toAddress, this.state.fromAddress)
-    } 
-    // console.log(this.state.data)
-    
-    return (
-      <View style={styles.container}>
-        <SearchBar
-		containerStyle={searchBar}
-		lightTheme
-        showLoading
-        platform="ios"
-        cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-        placeholder='From...' 
-        onChangeText={(fromAddress) => this.setState({fromAddress})} />
-        
-		    <SearchBar
-		containerStyle={searchBar}
-		lightTheme
-        showLoading
-        platform="ios"
-        cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-        placeholder='To...' 
-        onChangeText={(toAddress) => this.setState({toAddress})} />
+	changeTab(tab) {
+		if (this.state.data === undefined) { return; }
+			
+		if (tab.i === price) {
+			this.setState({ sortBy: price });
+		} else if (tab.i === time) {
+			this.setState({ sortBy: time });
+		}
+	}
 
-        <Button
-          title="Find me a car!"
-          onPress={(state) => {
-            // data = resolveAddress(this.state.toAddress, this.state.fromAddress)
-            this.setState({ buttonPress: true});
-          }}
-        />
+	getSortedData() {
+		var data = [];
+		if (this.state.sortBy === price) {
+			data = this.state.data.sort((a, b) => a.estimated_cost_cents_max - b.estimated_cost_cents_max)
+		} else if (this.state.sortBy === time) {
+			data = this.state.data.sort((a, b) => a.estimated_duration_seconds - b.estimated_duration_seconds)
+		} else {
+			data = this.state.data;	
+		}
+		console.log(data);
+		return data;
+	}
 
+	swapAddress() {
+		var to = this.state.toAddress;
+		var from = this.state.fromAddress;
 
-		<Card> 
-    		{
-    			this.state.data.map((item, index) => (
-    			<ListItem
-    			key={index}
-    			// leftIcon={ {name: item.ride_hailing_service }}
-					roundAvatar
-          avatar={item.ride_hailing_service == "uber" ? 'https://banner2.kisspng.com/20180715/fje/kisspng-computer-icons-user-clip-art-uber-logo-transparent-5b4b0088e1dc35.0486796815316419929251.jpg' : 'http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c525.png'}  
-    			badge={{ value: "$" + item.estimated_cost_cents_min + '-' + item.estimated_cost_cents_max, textStyle: { color: 'orange' }, containerStyle: { marginTop: -20 } }}
-    			title={ item.display_name }
-    			subtitle={ "Estimated time: " + Math.floor(item.estimated_duration_seconds / 60) + ' minutes'}
-    			hideChevron
-    			/>
-    		))
-    		}
-		</Card>
-      </View>
-    );
+		this.setState({ toAddress: from });
+		this.setState({ fromAddress: to });
+	}
+
+	render() {
+		if (this.state.buttonPress) {
+			this.resolveAddress(this.state.toAddress, this.state.fromAddress)
+		} 
+		console.log(this.state.data);
+		return (
+			<View style={styles.container}>
+				<Container>
+					<View>
+						<Header hasTabs searchBar rounded span>
+							<View style={{ flex: 1, paddingLeft: 10}}>
+								<Content>
+									<Item>
+										<Input placeholder="Choose starting point..." value={this.state.fromAddress} onChangeText={(fromAddress) => this.setState({fromAddress})}/>
+									</Item>
+									<Item style={{ borderColor: 'transparent' }}>
+										<Input placeholder="Choose destination..." value={this.state.toAddress} onChangeText={(toAddress) => this.setState({toAddress})}/>
+									</Item>
+								</Content>
+							</View>
+							<View style={{flexDirection: "column", marginTop: 15 }}>
+								<Button transparent onPress={() => {this.swapAddress()}}>
+									<Icon name="swap" />
+								</Button>
+								<Button transparent onPress={(state) => { this.setState({ buttonPress: true })}}> 
+									<Icon name="search" />
+								</Button>
+							</View>
+						</Header>
+					</View>
+					<View style={{ height: 30}}>
+						<Tabs onChangeTab={( tab ) => { this.changeTab(tab) }}>
+							<Tab heading={ <TabHeading><Icon name="cash" /></TabHeading>}  />
+							<Tab heading={ <TabHeading><Icon name="time" /></TabHeading>} />
+						</Tabs>
+					</View>
+					<View>
+						<List contentContainerStyle={{ marginTop: 100}}>
+						<FlatList
+						data = { this.getSortedData() }
+						keyExtractor={item => item.display_name + item.estimated_duration_seconds}
+						renderItem = {({ item }) => (
+							<ListItem
+							title={ item.display_name }
+							subtitle={ `ETA: ${Math.floor(item.estimated_duration_seconds / 60)} minutes\n\$${item.estimated_cost_cents_min}-${item.estimated_cost_cents_max}`}
+										avatar={item.ride_hailing_service == "uber" ? uberIcon : lyftIcon }  
+										subtitleNumberOfLines={2}
+										hideChevron
+										/>
+									)}
+								/>
+						</List>
+					</View>
+			  </Container>
+		  </View>
+  );
   }
 }
 
@@ -103,16 +143,11 @@ function resolveDestination(address) {
 	console.log("destination:", address);
 }
 
-let primaryColor = '#015385';
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'white',
-        marginTop: 40,
-    },
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		marginTop: 40,
+	},
 });
 
-const searchBar = {
-};
