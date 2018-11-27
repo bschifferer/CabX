4ae36ed2d667a78bcb8fe6b54e6c2075c3e2fb8e
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-11-27 18:29:55 -0500

    fix push

--- push.sh
+++ push.sh
@@ -6,13 +6,6 @@ setup_git() {
 }
 
 commit_website_files() {
-  cp -r coverage/* /tmp/
-  cp -r report_unittest.txt /tmp/
-  cp -r report_enslint.txt /tmp/
-  cp -r cp report_coverage.txt /tmp/
-  git stash save --keep-index --include-untracked
-  git pull origin coverage_reports
-  git checkout -b coverage_reports
   mkdir -p "reports" 
   if [ -z "$TRAVIS_BUILD_NUMBER" ]
     then
@@ -20,12 +13,12 @@ commit_website_files() {
   fi
   mkdir -p reports/${TRAVIS_BUILD_NUMBER}
   mkdir -p reports/${TRAVIS_BUILD_NUMBER}/coverage
-  cp -r /tmp/coverage/* reports/${TRAVIS_BUILD_NUMBER}/coverage/
-  cp /tmp/report_unittest.txt reports/${TRAVIS_BUILD_NUMBER}/report_unittest.txt
-  cp /tmp/report_enslint.txt reports/${TRAVIS_BUILD_NUMBER}/report_enslint.txt
-  cp /tmp/report_coverage.txt reports/${TRAVIS_BUILD_NUMBER}/report_coverage.txt
+  cp -r coverage/* reports/${TRAVIS_BUILD_NUMBER}/coverage/
+  cp report_unittest.txt reports/${TRAVIS_BUILD_NUMBER}/report_unittest.txt
+  cp report_enslint.txt reports/${TRAVIS_BUILD_NUMBER}/report_enslint.txt
+  cp report_coverage.txt reports/${TRAVIS_BUILD_NUMBER}/report_coverage.txt
   git add -- ./reports
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip] skip travis [skip travis-ci]"
 }
 
 upload_files() {
