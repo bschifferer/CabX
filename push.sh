c1629abd7a70fd7c9b6776f0605478c5d4ae4d4a
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-11-27 20:09:37 -0500

    fix push

--- push.sh
+++ push.sh
@@ -22,7 +22,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
+  git remote add master https://${GH_TOKEN}@github.com/bazile-clyde/CabX.git > /dev/null 2>&1
   git push --quiet 
 }
 
