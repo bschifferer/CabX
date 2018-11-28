6ec4347375ab219d7063dbf5f3f98bd004e43329
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-11-27 21:36:37 -0500

    example branch tests

---
+++ backend/branch_src_example/sample_ifelseifelseifelse.js
@@ -0,0 +1,18 @@
+exports.sample_ififeslse = function(blstr, blstr2, blstr3) {
+	if(blstr) {
+		console.log('if 1');
+	} else  {
+		console.log('else 1');
+	}
+	if(blstr2) {
+		console.log('if 2');
+	} else  {
+		console.log('else 2');
+	}
+	if(blstr3) {
+		console.log('if 3');
+	} else  {
+		console.log('else 3');
+	}
+	return(blstr)
+}
---
+++ backend/branch_src_example/sample_ififelse.js
@@ -0,0 +1,8 @@
+exports.sample_ififeslse = function(blstr, blstr2) {
+	if(blstr) {
+		console.log('if');
+	} else if (blstr2) { 
+		console.log('else');
+	}
+	return(blstr)
+}
