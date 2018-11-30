cbc8c8456e2d9d954489efabc798e35175d094d1
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-11-30 12:16:55 -0500

    test errors

---
+++ backend/src/sample_error.js
@@ -0,0 +1,13 @@
+exports.sample_error = function(blstr, blstr2, unused_var, unused_var_2) {
+	console.log(z);
+	var z = 0;
+	var foo;
+	console.log(foo === undefined);
+	if(blstr) {
+		
+	} else if (blstr2) { 
+		
+	}
+	notdeffunc();
+	return(x)
+}
