af2606234b85744f596427b04a154acdaf3dde15
Clyde Bazile <clydebazile@Clydes-MacBook-Pro.local>
2018-11-29 20:58:25 -0500

    Allow users to logout and logins to persist

--- frontend/cabx/App.js
+++ frontend/cabx/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Container } from "native-base";
 import PropTypes from 'prop-types';
-import { Stitch, AnonymousCredential } from 'mongodb-stitch-react-native-sdk';
+import { Stitch, UserPasswordCredential } from 'mongodb-stitch-react-native-sdk';
 
 import CabXHeader from './components/CabXHeader';
 import CabXTabs from './components/CabXTabs';
@@ -16,11 +16,48 @@ export default class App extends React.Component {
 		this.state = { 
 			data: [], 
 			isLoggedIn: false,
+			logout: false,
+			client: undefined,
+			currentUserId: undefined,
 		};
 	}
 
-	loginHandler = () => {
-		this.setState({ isLoggedIn: true });
+	componentDidMount() {
+		this._loadClient();
+	}
+
+	_loadClient = () => {
+		Stitch.initializeDefaultAppClient('cabxbackend-nddiu').then(client => {
+			this.setState({ client });
+
+			if(client.auth.isLoggedIn) {
+				this.setState({ currentUserId: client.auth.user.id, isLoggedIn: true });
+			}
+		});
+	}
+
+	_onPressLogin = (username, password) => {
+		return this.state.client.auth.loginWithCredential(new UserPasswordCredential(username, password)).then(user => {
+			const msg = `Successfully logged in as user ${user.id}`;
+			this.setState({ currentUserId: user.id })
+			this.setState({ isLoggedIn: true })
+			return { message: msg }
+		}).catch(err => {
+			const msg = `Failed to log in anonymously: ${err}`;
+			this.setState({ currentUserId: undefined })
+			return { message: msg, error : true }
+		});
+	}
+
+	_onPressLogout = () => {
+		this.state.client.auth.logout().then(user => {
+			console.log(`Successfully logged out`);
+			this.setState({ currentUserId: undefined })
+			this.setState({ isLoggedIn: false });
+		}).catch(err => {
+			console.log(`Failed to log out: ${err}`);
+			this.setState({ currentUserId: undefined })
+		});
 	}
 
 	searchHandler = (start, destination) => {
@@ -56,15 +93,15 @@ export default class App extends React.Component {
 		const isLoggedIn = this.state.isLoggedIn;
 		let display;
 		if (isLoggedIn) {
-			display = (<View style={styles.container}>
+			display = (
 				<Container>
-					<CabXHeader onSearch={this.searchHandler} />
+					<CabXHeader logout={this._onPressLogout} onSearch={this.searchHandler} />
 					<CabXTabs onChangeTab={this.tabHandler} />
 					<CabXList data={this.state.data} />
 				</Container>
-			</View>);
+			);
 		} else {
-			display = <LoginPage login={this.loginHandler} />;
+			display = <LoginPage login={this._onPressLogin} />;
 		}
 		
 		return (
@@ -79,14 +116,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		flexDirection: 'column',
-		marginTop: 40,
+		paddingTop: 40,
 	},
 });
 
-App.propTypes = {
-	_isMounted: PropTypes.bool.isRequired,
-}
-
-App.defaultProps = {
-	_isMounted: false
-}
