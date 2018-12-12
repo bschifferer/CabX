a987cedc38e11489cd906e49376fee9094fe0937
“bschifferer” <benedikt.d.schifferer@gmail.com>
2018-12-12 15:34:31 -0500

    email validation, nicer UI

--- frontend/cabx/components/LoginPage.js
+++ frontend/cabx/components/LoginPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
 import { Icon, Left, Right, Body, Title, Container, Button, Header, Content, Form, Item, Input, Label } from 'native-base';
 
+let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
 
 export default class LoginPage extends React.Component {
 	constructor(props) {
@@ -11,15 +12,18 @@ export default class LoginPage extends React.Component {
 			signUpPage: false,
 			loginFailure: false,
 			createAccountFailure: false,
+			createEmailFailure: false,
 			username: '',
 			password: '', 
 			confirmPassword: '',
 		};
 	}
 
+
+
 	toggleSignUpPage = () => {
 		let signUpPage = this.state.signUpPage;
-		this.setState({ signUpPage: !signUpPage, createAccountFailure: false, loginFailure: false });	
+		this.setState({ signUpPage: !signUpPage, createAccountFailure: false, loginFailure: false, createEmailFailure: false });	
 	}
 
 	login = async (username, password) => {
@@ -31,16 +35,24 @@ export default class LoginPage extends React.Component {
 	}
 
 	createAccount = async (username, password, confirmPassword) => {
-		const res = await this.props.createAccount(username, password);
-		console.log(res.message);
-		if (res.error) {
+		if (password != confirmPassword) {
 			this.setState({ createAccountFailure: true });
+		} else {
+			console.log(reg.test(username));
+			if (!reg.test(username)) {
+				this.setState({ createEmailFailure: true });
+			} else {
+				const res = await this.props.createAccount(username, password);
+				console.log(res.message);
+				this.toggleSignUpPage();
+			}
 		}
 	}
 
 	render() {
 			let loginFailure = this.state.loginFailure && !this.state.signUpPage;
 			let createAccountFailure = this.state.createAccountFailure && this.state.signUpPage;
+			let createEmailFailure = this.state.createEmailFailure && this.state.signUpPage;
 
 			return (
 				<Container>
@@ -53,10 +65,10 @@ export default class LoginPage extends React.Component {
 					</Header>
 					<Content style={{ marginRight: 5 }}>
 						<Form>
-							<Item floatingLabel error={ loginFailure }>
-								<Label>Username</Label>
+							<Item floatingLabel error={ loginFailure || createEmailFailure }>
+								<Label>E-Mail</Label>
 								<Input autoCapitalize='none' onChangeText={(text) => { this.setState({ username: text })}}/>
-								{ loginFailure && <Icon name='close-circle' /> }
+								{ (loginFailure || createEmailFailure) && <Icon name='close-circle' /> }
 							</Item>
 							<Item floatingLabel error={ loginFailure || createAccountFailure }>
 								<Label>Password</Label>
